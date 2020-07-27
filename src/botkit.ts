465a231d881670db747425f5be2749a63f942795
Stephen A <stephen@creativelogic.me>
2020-07-27 17:09:23 -0500

    debug

--- src/botkit.ts
+++ src/botkit.ts
@@ -38,9 +38,11 @@ async function middlewareDelay(bot: BotWorker & { api: any }, message: { text: s
     console.log(`locked ${activity.conversation.id}`);
 
     await bot.api.callAPI('sendChatAction', 'POST', { chat_id: activity.conversation.id, action: "typing" });
-    await setTimeout(async ()=> { await next(); }, time);
-    // processing[activity.conversation.id] = false;
-    console.log(`unlocked ${activity.conversation.id}`);
+    await setTimeout(async () => {
+      // processing[activity.conversation.id] = false;
+      console.log(`unlocked ${activity.conversation.id}`);
+      await next();
+    }, time);
   } else {
     await next();
   } 
